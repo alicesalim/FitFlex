a5e48a507eda22e9d41cd606ca990f9636f81174
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-01-28 16:43:15 -0300

    Update - 01
    + Adicionei a pasta images dentro da pasta assets
    ! Realizei alterações no CSS (button.module.css) para deixar com o visual igual ao do site antigo.
    + Realizei alterações no CSS (home.modules.css) para deixar com o visual do site antigo
    ! Ainda preciso corrigir a posição da imagem de botão e acrescentar o carrossel de imagens.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import React from "react"
-import Button from '../components/button/button'
-import style from './Home.module.css'
+import React from "react";
+import style from './Home.module.css';
+import Button from '../components/button/Button'; // Certifique-se de importar o componente Button
 
-function Home (){
-    return(
+const Home = () => {
+    return (
+      <div className={style.inicial}>
         <div className={style.container}>
-            <h1> Bem Vindo ao FitFlex!</h1>
-            <p>Acompanhe suas dietas e explore substituições saudáveis para suas receitas favoritas</p>
-            <Button text="Substituir" />
-        </div>       
-    )
-} export default Home;
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+            <div>
+              <h1 className={style.display4}>Bem-vindo ao FitFlex!</h1>
+              <p className={style.pagtxt}>
+                Acompanhe suas dietas e explore substituições saudáveis para suas receitas favoritas.
+              </p>
+              <img src="/src/assets/images/paginicial.jpg" className={style.imgFluid} alt="Imagem de destaque" />
+            </div>
+          </div>
+          <Button text="Substituir receita" />
+        </div>
+      </div>
+    );
+};
+
+export default Home;
